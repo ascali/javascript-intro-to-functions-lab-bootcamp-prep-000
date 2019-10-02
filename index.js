be72dd4f33df0da26a8a3eb962f2f5c853728670
Ascaliko <ascaliko7@gmail.com>
2019-10-02 04:56:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,15 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
+  var str = ''
+  if(string==='hello'){
+    str = "I can't hear you!"
+  }
+  if(string==='HELLO'){
+    str = "YES INDEED!"
+  }
+  if(string==='I love you, Grandma.'){
+    str = "I love you, too."
+  }
+  return str
 }
